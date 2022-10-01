9afe48ca6982d3fa6acc902385382f7a98c6ba78
dondoniz <leonardosiqueira387@gmail.com>
2022-10-01 20:30:11 -0300

    later

---
+++ Exercicio22Lista4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Exercicio22Lista4 {
+    public static void main(String args[]){
+
+    Scanner input = new Scanner(System.in);
+
+    /*
+     *  Faça um programa que leia um valor inteiro e positivo (garanta isso),
+    calcule e escreva o seu somatório. O cálculo do somatório deve ser feito
+    por meio de um método.
+     */
+    
+
+    }
+    
+}
