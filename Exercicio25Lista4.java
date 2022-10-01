1ab788661b63607e65d8222cf0b8ca217101b9e3
dondoniz <leonardosiqueira387@gmail.com>
2022-10-01 20:29:33 -0300

    studying again

---
+++ Exercicio25Lista4.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Exercicio25Lista4 {
+    public static void main(String args[]) {
+
+        Scanner input = new Scanner(System.in);
+
+        /*
+         * Implemente um programa semelhante ao 24, ele também deve ler um valor
+         * inteiro e positivo (garanta isso), mas calcular e devolver a quantidade de
+         * divisores desse valor. A contagem dos divisores deve ser feita por um
+         * método.
+         */
+
+        System.out.println("Informe um número inteiro e positivo: ");
+        int num = input.nextInt();
+        if (num < 1) {
+            System.out.println("ERRO! Informe um número inteiro e positivo.");
+            return;
+        }
+
+        int divisores = divisores(num);
+
+        System.out.println("Quantidade de divisores: " + divisores);
+
+    }
+
+    public static int divisores(int num) {
+
+        int divisores = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                divisores++;
+            }
+
+        }
+        return divisores;
+    }
+
+}
