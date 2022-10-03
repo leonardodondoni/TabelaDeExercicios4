40a5a81e7cfb0afce765baa6e8b6c81c337f0721
dondoniz <leonardosiqueira387@gmail.com>
2022-10-03 19:52:16 -0300

    updateee

--- Operacoes.java
+++ Operacoes.java
@@ -20,5 +20,15 @@ public class Operacoes {
         }
         return divisores;
     }
+    public static void divisoresTela(int num) {
 
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.println(i);      
+          }
+
+        }
+    
+    }
 }
