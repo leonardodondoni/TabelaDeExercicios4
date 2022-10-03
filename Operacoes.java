3ff435cb4a2d3b53ca57a963663d794f5a181998
dondoniz <leonardosiqueira387@gmail.com>
2022-10-03 19:35:05 -0300

    metodo classe

---
+++ Operacoes.java
@@ -0,0 +1,11 @@
+public class Operacoes {
+    public static int somatorio(int num){
+
+        int somatorio = 0;
+        for(int i = 1;i <= num; i++){
+            somatorio = somatorio + i;
+        }
+        return somatorio;
+    }
+    
+}
