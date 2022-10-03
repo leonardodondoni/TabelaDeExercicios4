b0e334d07cf6f9bd81433b595e17d737a093889d
dondoniz <leonardosiqueira387@gmail.com>
2022-10-03 19:27:44 -0300

    updates!

--- Exercicio22Lista4.java
+++ Exercicio22Lista4.java
@@ -8,9 +8,29 @@ public class Exercicio22Lista4 {
      *  Faça um programa que leia um valor inteiro e positivo (garanta isso),
     calcule e escreva o seu somatório. O cálculo do somatório deve ser feito
     por meio de um método.
-     */
+    */
+
+    System.out.println("Informe um valor inteiro e positivo: ");
+    int num = input.nextInt();
+    if (num < 1){
+        System.out.println("ERRO! Informe um valor inteiro e positivo.");
+        return;
+    }
+
+    int somatorio = somatorio(num);
+
+    System.out.println("Somatório do valor: " + somatorio);
+
     
 
+    }
+    public static int somatorio(int num){
+
+        int somatorio = 0;
+        for(int i = 1;i <= num; i++){
+            somatorio = somatorio + i;
+        }
+        return somatorio;
     }
     
 }
