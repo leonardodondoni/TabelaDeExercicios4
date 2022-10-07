41d8eae2601a88c93dbaaa49f985155faa9ba8c8
Leonardo <leonardosiqueira387@gmail.com>
2022-10-06 22:37:25 -0300

    ok

---
+++ Exercicio7Recursao.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Exercicio7Recursao {
+    public static void main(String args[]){
+
+        Scanner input = new Scanner(System.in);
+
+        /*Faça um programa que leia um valor n qualquer (inteiro e positivo). 
+        O programa deve escrever o n-ésimo termo da série de Fibonacci. 
+        Os termos desse série são 1,1,2,3,5,8,13,21,... O n-ésimo termo deve ser calculado por um método recursivo.
+        */
+        
+    }
+    
+}
