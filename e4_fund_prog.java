5cd2842dde5521bd00364c40f4305286265439b8
Leonardo <leonardosiqueira387@gmail.com>
2022-10-12 21:19:38 -0300

    i did it

--- e4_fund_prog.java
+++ e4_fund_prog.java
@@ -11,25 +11,30 @@ public class e4_fund_prog {
 
         System.out.println("Informe um valor inteiro e positivo que corresponde a quantidade de termos");
         int n = input.nextInt();
-
+        if(n < 1){
+            System.out.println("ERRO! Informe um valor válido.");
+            return;
+        }
         System.out.println(iterativeE4(n));
+        System.out.println(recursiveE4(n));
 
     }
     public static double iterativeE4(int n){
         double plus2 = 3, plus4 = 4, term = 0, totalTerm = 0;
         if(n < 2) return 4.0/3;
         for(int i = 1; i <= n; i ++){
-            plus2 = plus2 + 2.0;
-            plus4 = plus4 + 4.0;
             term = plus4 / plus2;
-            totalTerm = totalTerm + term;
+            plus2 += 2;
+            plus4 += 4;
+            
+            totalTerm += term;
         }
        
         return totalTerm;
     }
     public static double recursiveE4(int n){
         if(n < 2) return 4.0/3;
-        return recursiveE4(n - 1) + ((n + 4.0) / (n + 2.0));
+        return recursiveE4(n - 1) + ((n * 4.0) / (n * 2.0 + 1));
        
     }
 }
